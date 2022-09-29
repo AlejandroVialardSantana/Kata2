1e14e5b6f68670c7fd994fd7de0535c68cdeb7ff
Alejandro Vialard <alejandrovialard2002@gmail.com>
2022-09-29 22:13:18 -0100

    Versión 4 Kata2

--- src/main/java/is2/kata2_v20220929/HistogramGenerator.java
+++ src/main/java/is2/kata2_v20220929/HistogramGenerator.java
@@ -3,15 +3,15 @@ package is2.kata2_v20220929;
 import java.util.HashMap;
 import java.util.Map;
 
-public class HistogramGenerator {
-    private final int[] data;
+public class HistogramGenerator<T> {
+    private final T[] data;
 
-    public HistogramGenerator(int[] data) {
+    public HistogramGenerator(T[] data) {
         this.data = data;
     }
     
-    Map<Integer, Integer> getHistogram() {
-        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+    Map<T, Integer> getHistogram() {
+        Map<T, Integer> map = new HashMap<T, Integer>();
         
         for (int i = 0; i < data.length; i++) {
             map.put(data[i], map.containsKey(data[i]) ? map.get(data[i]) + 1 : 1);
